a392a75c68d5ea41b39e6d252f33d8983cdd9d93
DaazKu <daazku@gmail.com>
2017-01-05 16:28:42 -0500

    test

--- applications/vanilla/controllers/class.vanillasettingscontroller.php
+++ applications/vanilla/controllers/class.vanillasettingscontroller.php
@@ -1011,7 +1011,7 @@ class VanillaSettingsController extends Gdn_Controller {
      * Sorting display order of categories.
      */
     public function minimalBug() {
-        CategoryModel::rebuildTree(true);
+        $this->CategoryModel->rebuildTree(true);
     }
 }
 
